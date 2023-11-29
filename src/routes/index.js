37f50daa3fc4eb357b0fd12ac9dc30934af0f643
Emad Anwer <emadanwer.official@gmail.com>
2023-11-29 17:50:08 +0200

    add users route to routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import roomsRouter from "./rooms.js";
 import filtersRouter from "./filters.js";
+import usersRouter from "./users.js";
 const router = Router();
 router.use("/rooms", roomsRouter);
 router.use("/filters", filtersRouter);
-
+router.use("/users", usersRouter);
 export default router;
